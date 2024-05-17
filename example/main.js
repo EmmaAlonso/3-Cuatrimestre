3b299dd892554842fc50244ae7036500194c347a
emmanuel010205 <emmanuel010205@gmail.com>
2024-05-17 19:16:17 +0000

    Primer ejemplo usando IDX

--- example/main.js
+++ example/main.js
@@ -1,2 +1,9 @@
 console.log("hola")
 console.log("Bienvenidos a la clase de programas")
+
+/*
+--Pasos para mandar cambios a repositorio desde terminal
+1.-git add .
+2.-git commit -m "Mensaje de lo que mandas al repo"
+3.-git push origin master
+*/
