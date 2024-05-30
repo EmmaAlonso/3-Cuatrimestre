bb3b70a99b01f5ad04561a27fd04be938649ec99
EmmaAlonso <159556204+EmmaAlonso@users.noreply.github.com>
2024-05-30 09:05:38 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# Suma de dos numeros
+
+numero1= float(input("Introducir numero 1: "))
+numero2= float(input("Introducir numero 2: ")) 
+suma= numero1+numero2
+
+# Resultado
+print("La suma de los dos numeros es: ",suma)
---
+++ main2 (1).py
@@ -0,0 +1,9 @@
+# Numero par o impar
+
+numero= int(input("introducir numero: "))
+
+if numero % 2 == 0:
+
+    print(numero, "Es un numero par")
+else:
+    print(numero, "Es un numero impar")
